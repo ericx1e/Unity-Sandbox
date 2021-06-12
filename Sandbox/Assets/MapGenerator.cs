2683e5201f09a4ffeab8c656a12a634ccd6c3cac
ericx1e <ericxie6@gmail.com>
2021-06-12 01:41:35 -0400

    noise map generation

---
+++ Sandbox/Assets/MapGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public int octives;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octives, persistance, lacunarity, offset);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+
+    }
+
+    //runs whenever a variable is changed in the inspector
+    void OnValidate()
+    {
+        if(mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if(lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if(octives < 0)
+        {
+            octives = 0;
+        }
+    }
+}
